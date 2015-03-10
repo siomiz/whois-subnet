7baeb2a4a2d1830023588ddaa558fc3231b89023
Tomohisa Kusano <siomiz@gmail.com>
2015-03-10 13:03:30 -0700

    initial commit

---
+++ entrypoint.js
@@ -0,0 +1,23 @@
+#!/usr/local/bin/node
+
+if(process.argv.length < 2){
+  process.exit(-1);
+}
+
+var IPADDR = process.argv[2];
+
+var whois = require('node-whois');
+var isc = require('ip-subnet-calculator');
+
+whois.lookup(IPADDR, function(err, data){
+  var lines = data.split("\n");
+  for(n in lines){
+    if(lines[n].match(/^inetnum|NetRange/)){
+      var out = lines[n].match(/([0-9\.]+)/g);
+      var subnets = isc.calculate(out[0], out[1]);
+      for(s in subnets){
+        console.log(subnets[s].ipLowStr + '/' + subnets[s].prefixSize);
+      }
+    }
+  }
+});
